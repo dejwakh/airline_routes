00fe15ff0ece084016f5b4fd65cfe6082dc7c967
SrdjanCoric <srdjan.coric1984@gmail.com>
2020-10-09 17:07:09 +0200

    boilerplate airlines

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="app">
+  <header className="header">
+    <h1 className="title">Airline Routes</h1>
+  </header>
+  <section>
+    <p>
+      Welcome to the app!
+    </p>
+  </section>
+</div>
+)
+
+export default App;
